9b0adf93479a9afcfc9712a847b4c042a3c1816b
Ma <z2715674@gmail.com>
2025-01-14 08:10:38 +0800

    update 3110. Score of a String

--- 3110. Score of a String/solution.py
+++ 3110. Score of a String/solution.py
@@ -18,6 +18,7 @@
 # Output: 50
 # Explanation (解釋):
 # The ASCII values of the characters in s are: 'z' = 122, 'a' = 97. So, the score of s would be |122 - 97| + |97 - 122| = 25 + 25 = 50.
+
 # Constraints (限制):
 # 2 <= s.length <= 100
 # s consists only of lowercase English letters.
@@ -41,58 +42,57 @@ class Solution:
 # 計算相鄰字元的 ASCII 差的絕對值，並將結果累加到 total 並回傳
 
 
-# sum()
 # class Solution:
 #     def scoreOfString(self, s: str) -> int:
 #         return sum(abs(ord(s[i]) - ord(s[i + 1])) for i in range(len(s) - 1))
 
+# sum() 寫法
 # sum() 將生成式的每個元素累加
 # 生成式使用 for 迴圈生成每個相鄰字元的 ASCII 絕對差
 
 
-# zip()
 # class Solution:
 #     def scoreOfString(self, s: str) -> int:
 #         return sum(abs(ord(a) - ord(b)) for a, b in zip(s, s[1:]))
 
+# zip() 寫法
 # zip() 將兩個序列按照索引配對
 # s 為原字串，s[1:] 為去掉第一個字元的字串
 # 生成式將配對的相鄰字元計算 ASCII 絕對差並累加
 
 
-# itertools.pairwise (Python 3.10)
 # from itertools import pairwise
 
 # class Solution:
 #     def scoreOfString(self, s: str) -> int:
 #         return sum(abs(ord(a) - ord(b)) for a, b in pairwise(s))
 
+# itertools.pairwise (Python 3.10)
 # pairwisw() 將序列中的相鄰元素自動配對
 # 只支援 Python 3.10 以上的版本
 
 
-# map()
 # class Solution:
 #     def scoreOfString(self, s: str) -> int:
 #         differences = map(lambda pair: abs(ord(pair[0]) - ord(pair[1])), zip(s, s[1:]))
 #         return sum(differences)
 
+# map() 寫法
 # map() 將每個配對的相鄰字元應用 lambda 函式
 # zip() 將字串的每個相鄰字元配對
 
 
-# 遞迴
 # class Solution:
 #     def scoreOfString(self, s: str) -> int:
 #         if len(s) == 1:
 #             return 0
 #         return abs(ord(s[0]) - ord(s[1])) + self.scoreOfString(s[1:])
 
+# 遞迴寫法
 # 遞迴呼叫自身來處理剩餘的字串
 # 每次遞迴計算第一個相鄰字元的 ASCII 絕對差，對剩餘的字串重複過程。
 
 
-# Numpy
 # import numpy as np
 
 # class Solution:
@@ -100,6 +100,7 @@ class Solution:
 #         ascii_vals = np.array(list(map(ord, s)))
 #         return np.sum(np.abs(np.diff(ascii_vals)))
 
+# Numpy 寫法
 # map(ord, s) 將字串轉成 ASCII 值列表
 # np.array() 將列表轉成 Numpy 陣列
 # np.diff() 計算陣列中相鄰元素的差
