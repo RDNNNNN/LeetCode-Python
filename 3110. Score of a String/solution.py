0583dcd3c49facc3075bc1101edc5ecf92cd8c8d
Ma <z2715674@gmail.com>
2025-01-14 13:25:18 +0800

    update 3110. Score of a String

--- 3110. Score of a String/solution.py
+++ 3110. Score of a String/solution.py
@@ -1,25 +1,25 @@
-# 3110. Score of a String (字串分數)
+# 3110. Score of a String 字串分數
 
 # You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
 # 給定一個字串 s，字串的分數定義為相鄰字元的 ASCII 值之間的絕對差之和。
 # Return the score of s.
 # 回傳 s 的分數
 
-
+# 範例
 # Example 1:
 # Input: s = "hello"
 
 # Output: 13
-# Explanation (解釋):
+# Explanation 解釋:
 # The ASCII values of the characters in s are: 'h' = 104, 'e' = 101, 'l' = 108, 'o' = 111. So, the score of s would be |104 - 101| + |101 - 108| + |108 - 108| + |108 - 111| = 3 + 7 + 0 + 3 = 13.
 # Example 2:
 # Input: s = "zaz"
 
 # Output: 50
-# Explanation (解釋):
+# Explanation 解釋:
 # The ASCII values of the characters in s are: 'z' = 122, 'a' = 97. So, the score of s would be |122 - 97| + |97 - 122| = 25 + 25 = 50.
 
-# Constraints (限制):
+# Constraints 限制:
 # 2 <= s.length <= 100
 # s consists only of lowercase English letters.
 # s 僅由小寫字母組成
