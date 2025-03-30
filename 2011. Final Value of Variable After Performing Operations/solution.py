9ceaeac43bd626f3bdf99bd4711751dc80f78c9e
Ma <z2715674@gmail.com>
2025-03-30 16:24:39 +0800

    update 2011. Final Value of Variable After Performing Operations

--- 2011. Final Value of Variable After Performing Operations/solution.py
+++ 2011. Final Value of Variable After Performing Operations/solution.py
@@ -12,78 +12,55 @@
 # 給定一個包含操作清單的字串操作陣列，執行所有操作後傳回 X 的最終值
 
 # Example 1 範例:
-
 # Input: operations = ["--X","X++","X++"]
-
 # Output: 1
 
 # Explanation 解釋:
 # The operations are performed as follows 操作如下:
-
 # Initially, X = 0.
 # 最初 X = 0
-
 # --X: X is decremented by 1, X =  0 - 1 = -1.
 # --X: X 減少 1， X = 0 - 1 = -1
-
-
 # X++: X is incremented by 1, X = -1 + 1 =  0.
 # X++: X 增加 1，X = -1 + 1 = 0
-
 # X++: X is incremented by 1, X =  0 + 1 =  1.
 # X++: X 增加 1，X = 0 + 1 = 1
 
 # Example 2 範例:
-
 # Input: operations = ["++X","++X","X++"]
-
 # Output: 3
 
 # Explanation 解釋:
 # The operations are performed as follows 操作如下:
-
 # Initially, X = 0.
 # 最初 X = 0
-
 # ++X: X is incremented by 1, X = 0 + 1 = 1.
 # ++X：X 增加 1，X = 0 + 1 = 1
-
 # ++X: X is incremented by 1, X = 1 + 1 = 2.
 # ++X：X 增加 1，X = 1 + 1 = 2
-
 # X++: X is incremented by 1, X = 2 + 1 = 3.
 # X++：X 增加 1，X = 2 + 1 = 3
 
 # Example 3 範例:
-
 # Input: operations = ["X++","++X","--X","X--"]
-
 # Output: 0
 
 # Explanation 解釋:
 # The operations are performed as follows 操作如下:
-
 # Initially, X = 0.
 # 最初 X = 0
-
 # X++: X is incremented by 1, X = 0 + 1 = 1.
 # X++：X 增加 1，X = 0 + 1 = 1
-
 # ++X: X is incremented by 1, X = 1 + 1 = 2.
 # ++X：X 增加 1，X = 1 + 1 = 2
-
 # --X: X is decremented by 1, X = 2 - 1 = 1.
 # --X：X 減少 1，X = 2 - 1 = 1
-
 # X--: X is decremented by 1, X = 1 - 1 = 0.
 # X--：X 減少 1，X = 1 - 1 = 0
 
 # Constraints 限制:
-
 # 1 <= operations.length <= 100
-
 # operations[i] will be either "++X", "X++", "--X", or "X--"
-
 # operations[i] 將是 ++X、X++、--X 或 X--
 
 # Code
