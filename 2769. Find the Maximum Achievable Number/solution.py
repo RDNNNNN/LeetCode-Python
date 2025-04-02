77687469e3138029ea1dc9cb43d9f6dd94699104
Ma <z2715674@gmail.com>
2025-04-02 09:09:19 +0800

    update 2769. Find the Maximum Achievable Number

--- 2769. Find the Maximum Achievable Number/solution.py
+++ 2769. Find the Maximum Achievable Number/solution.py
@@ -1,9 +1,10 @@
 ## 2769. Find the Maximum Achievable Number 找到可實現的最大數字
 
 # Given two integers, num and t. A number x is achievable if it can become equal to num after applying the following operation at most t times:
-# 給兩個整數 num 跟 t，如果數字 x 能在最多 t 次運算後為 num，則該數字是可實現的
 # Increase or decrease x by 1, and simultaneously increase or decrease num by 1.
 # Return the maximum possible value of x.
+
+# 給兩個整數 num 跟 t，如果數字 x 能在最多 t 次運算後為 num，則該數字是可實現的
 # 將 x 增加或減少 1 ，同時將 num 增加或減少 1，回傳 x 可能的最大值
 
 ### 懶人包
@@ -16,8 +17,9 @@
 
 ### Explanation 解釋:
 # Apply the following operation once to make the maximum achievable number equal to num:
-# 使用以下操作一次，使最大的可實現數字為 num
 # Decrease the maximum achievable number by 1, and increase num by 1.
+
+# 使用以下操作一次，使最大的可實現數字為 num
 # 將最大的可實現數量減少 1，並將數量增加 1
 
 ### Example 2 範例:
@@ -26,8 +28,9 @@
 
 ### Explanation 解釋:
 # Apply the following operation twice to make the maximum achievable number equal to num:
-# 使用以下操作兩次，使最大的可實現數字為 num
 # Decrease the maximum achievable number by 1, and increase num by 1.
+
+# 使用以下操作兩次，使最大的可實現數字為 num
 # 將最大的可實現數量減少 1，並將數量增加 1
 
 ### Constraints 限制:
